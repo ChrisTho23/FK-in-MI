876390a5c9f35f587bdd1ca780ba438fe580daa4
ChrisTho23 <110739558+ChrisTho23@users.noreply.github.com>
2024-08-27 19:34:05 -0700

    Direct logit attribution (#2)
    * Add pydantic dataset for dataset generation
    
    * Add placeholder for OpenAI API Key
    
    * Add Fasle to answer map
    
    * Update dataframe used for experiment
    
    * Add section on direct logit attribution
    
    * Add scripts for direct logit attribution

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,6 @@
-import transformer_lens.utils as utils
 import plotly.express as px
+import transformer_lens.utils as utils
+from transformer_lens import HookedTransformer
 
 
 def line(tensor, **kwargs):
@@ -7,3 +8,10 @@ def line(tensor, **kwargs):
         y=utils.to_numpy(tensor),
         **kwargs,
     ).show()
+
+def answer_map_to_tokens(model: HookedTransformer, answer_map: dict):
+    answer_map_tokens = {
+        key:model.to_tokens(value, prepend_bos=False).squeeze() for key, value in answer_map.items()
+    }
+
+    return answer_map_tokens
